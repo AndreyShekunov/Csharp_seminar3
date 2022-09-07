34ddcc8527e807a4b594b4ae9580431c9fa4c5ff
Шекунов Андрей <asheknov@gmail.com>
2022-09-07 19:45:05 +0500

    Задача 18 показать диапазон координат по номеру четверти

---
+++ task18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер координатной четверти от 1 до 4:");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if(quarter > 0 && quarter < 5)
+{
+    if(quarter == 1)
+    {
+        Console.WriteLine("Диапазон: х > 0 и y > 0");
+    }
+    if(quarter == 2)
+    {
+        Console.WriteLine("Диапазон: х < 0 и y > 0");
+    }
+    if(quarter == 3)
+    {
+        Console.WriteLine("Диапазон: х < 0 и y < 0");
+    }
+    if(quarter == 4)
+    {
+        Console.WriteLine("Диапазон: х > 0 и y < 0");
+    }
+}
+else
+{
+    Console.WriteLine("Существует только 4 координатные четверти");
+}

c5b3e8ae5616d3696452d281390f43eb436fb4f0
Шекунов Андрей <asheknov@gmail.com>
2022-09-10 22:38:17 +0500

    additional_task доп. задание - метод для разворота массива

---
+++ additional_task/Program.cs
@@ -0,0 +1,36 @@
+﻿// Доп. задание
+// Написать метод для разворота массива,
+// не используя память под дополнительный массив
+
+int[] ArrayRev(int[] array) // метод разворота массива
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+         int temp = array[i];
+         array[i] = array[array.Length - 1 - i];
+         array[array.Length - 1 - i] = temp;
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // метод вывода массива в терминал в строку
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+Console.WriteLine();
+PrintArray(arr); // метод выводит исходный массив
+Console.WriteLine();
+ArrayRev(arr);
+PrintArray(arr); // метод выводит перевернутый массив
+
+
+
+

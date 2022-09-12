abecac465c2158d08af43a6bd07fd38b88802038
Шекунов Андрей <asheknov@gmail.com>
2022-09-12 18:29:24 +0500

    разворот числа с помощью цикла while

---
+++ reversNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// Разворот числа
+
+int number = 12345; // Исходное число
+int tempNumber = number; // Число, которое будем разворачивать
+int reversedNumber =0; // развернутое число
+while(tempNumber > 0)
+{
+    int digital = tempNumber % 10;
+    reversedNumber = reversedNumber * 10 + digital;
+    tempNumber = tempNumber / 10;
+}
+Console.WriteLine($"Развёрнутое число = {reversedNumber}");
